2ed0b03eb35c021ade7d2477d95aea2465d57ab0
jc-coder <josephchimezie2003@gmail.com>
2023-09-12 03:14:00 +0100

    update db connection variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { MongooseModule } from "@nestjs/mongoose";
-import * as dotenv from "dotenv";
-import { Person, PersonSchema } from "./person.schema";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+import { Person, PersonSchema } from './person.schema';
 dotenv.config();
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.DB_URL),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
     MongooseModule.forFeature([{ name: Person.name, schema: PersonSchema }]),
   ],
   controllers: [AppController],
